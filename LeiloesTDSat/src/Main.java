506185d995df3afe2ad77d6120382c3421208689
Bruno Moscan <bruno.moscan@icloud.com>
2024-08-24 11:42:09 -0300

    Adicionado classe Main, que agora abre a tela cadastroVIEW

---
+++ LeiloesTDSat/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        cadastroVIEW view = new cadastroVIEW(); //primeiro commit será por conta desta modificação
+        
+        view.setVisible(true);
+        
+        view.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+    }
+}
