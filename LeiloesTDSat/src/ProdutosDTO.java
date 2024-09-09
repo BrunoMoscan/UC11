4a5ae0b87f0d89035b045d2ccb907ba54965123c
Bruno Moscan <bruno.moscan@icloud.com>
2024-09-09 19:27:28 -0300

    Implementada função venderProduto em ProdutosDAO

--- LeiloesTDSat/src/ProdutosDTO.java
+++ LeiloesTDSat/src/ProdutosDTO.java
@@ -3,6 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Adm
@@ -46,3 +51,43 @@ public class ProdutosDTO {
     }
     
 }
+
+public void venderProduto(int produtoId) {
+    String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
+    try (Connection conn = this.connect();
+         PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        pstmt.setInt(1, produtoId);
+        pstmt.executeUpdate();
+    } catch (SQLException e) {
+        System.out.println(e.getMessage());
+    }
+}
+
+public List<Produto> listarProdutosVendidos() {
+    List<Produto> produtosVendidos = new ArrayList<>();
+    String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+    try (Connection conn = this.connect();
+         Statement stmt = conn.createStatement();
+         ResultSet rs = stmt.executeQuery(sql)) {
+
+        while (rs.next()) {
+            Produto produto = new Produto();
+            while (rs.next()) {
+                Produto produto = new Produto();
+            
+                produto.setId(rs.getInt("id"));           
+                produto.setNome(rs.getString("nome"));  
+                produto.setPreco(rs.getDouble("preco"));
+                produto.setStatus(rs.getString("status"));
+            
+                produtosVendidos.add(produto);
+            }
+            
+            produtosVendidos.add(produto);
+        }
+    } catch (SQLException e) {
+        System.out.println(e.getMessage());
+    }
+    return produtosVendidos;
+}
+
